4839f5595e5bfde94a6e6e6a09634a268cb71e21
Kelly Norton <kellegous@gmail.com>
2026-01-28 19:47:38 -0500

    clean up test utils

--- validate_test.go
+++ validate_test.go
@@ -10,96 +10,132 @@ import (
 	"github.com/kellegous/poop"
 )
 
-type Pattern struct {
-	Data    []byte
-	Desc    string
-	Compare func(got []byte, expected []byte) bool
+type Pattern interface {
+	Len() int
+	Desc() string
+	Compare(got []byte) bool
 }
 
-func Byte(b byte) Pattern {
-	return Pattern{
-		Data:    []byte{b},
-		Desc:    fmt.Sprintf("byte(%d)", b),
-		Compare: bytes.Equal,
+type LiteralPattern interface {
+	Pattern
+	Bytes() []byte
+}
+
+type fromBytes struct {
+	data []byte
+	desc string
+}
+
+var _ LiteralPattern = (*fromBytes)(nil)
+
+func (f *fromBytes) Len() int {
+	return len(f.data)
+}
+
+func (f *fromBytes) Desc() string {
+	return f.desc
+}
+
+func (f *fromBytes) Compare(got []byte) bool {
+	return bytes.Equal(got, f.data)
+}
+
+func (f *fromBytes) Bytes() []byte {
+	return f.data
+}
+
+type anyBytes struct {
+	len  int
+	desc string
+}
+
+var _ Pattern = (*anyBytes)(nil)
+
+func (a *anyBytes) Len() int {
+	return a.len
+}
+
+func (a *anyBytes) Desc() string {
+	return a.desc
+}
+
+func (a *anyBytes) Compare(got []byte) bool {
+	return len(got) == a.len
+}
+
+func Byte(b byte) LiteralPattern {
+	return &fromBytes{
+		data: []byte{b},
+		desc: fmt.Sprintf("byte(%d)", b),
 	}
 }
 
-func Command(c CommandCode) Pattern {
-	return Pattern{
-		Data:    []byte{byte(c)},
-		Desc:    fmt.Sprintf("command(%d)", c),
-		Compare: bytes.Equal,
+func Command(c CommandCode) LiteralPattern {
+	return &fromBytes{
+		data: []byte{byte(c)},
+		desc: fmt.Sprintf("command(%d)", c),
 	}
 }
 
-func Bytes(bs ...byte) Pattern {
-	return Pattern{
-		Data:    bs,
-		Desc:    fmt.Sprintf("bytes(%v)", bs),
-		Compare: bytes.Equal,
+func Bytes(bs ...byte) LiteralPattern {
+	return &fromBytes{
+		data: bs,
+		desc: fmt.Sprintf("bytes(%v)", bs),
 	}
 }
 
-func String(s string) Pattern {
-	return Pattern{
-		Data:    []byte(s),
-		Desc:    fmt.Sprintf("string(%q)", s),
-		Compare: bytes.Equal,
+func String(s string) LiteralPattern {
+	return &fromBytes{
+		data: []byte(s),
+		desc: fmt.Sprintf("string(%q)", s),
 	}
 }
 
-func Int32(i int32, e binary.ByteOrder) Pattern {
+func Int32(i int32, e binary.ByteOrder) LiteralPattern {
 	buf := make([]byte, 4)
 	e.PutUint32(buf, uint32(i))
-	return Pattern{
-		Data:    buf,
-		Desc:    fmt.Sprintf("int32(%d, %s)", i, e.String()),
-		Compare: bytes.Equal,
+	return &fromBytes{
+		data: buf,
+		desc: fmt.Sprintf("int32(%d, %s)", i, e.String()),
 	}
 }
 
-func Uint32(i uint32, e binary.ByteOrder) Pattern {
+func Uint32(i uint32, e binary.ByteOrder) LiteralPattern {
 	buf := make([]byte, 4)
 	e.PutUint32(buf, i)
-	return Pattern{
-		Data:    buf,
-		Desc:    fmt.Sprintf("uint32(%d, %s)", i, e.String()),
-		Compare: bytes.Equal,
+	return &fromBytes{
+		data: buf,
+		desc: fmt.Sprintf("uint32(%d, %s)", i, e.String()),
 	}
 }
 
-func Time(t time.Time, e binary.ByteOrder) Pattern {
+func Time(t time.Time, e binary.ByteOrder) LiteralPattern {
 	buf := make([]byte, 4)
 	e.PutUint32(buf, uint32(t.Unix()))
-	return Pattern{
-		Data:    buf,
-		Desc:    fmt.Sprintf("time(%s, %s)", t.Format(time.RFC3339), e.String()),
-		Compare: bytes.Equal,
+	return &fromBytes{
+		data: buf,
+		desc: fmt.Sprintf("time(%s, %s)", t.Format(time.RFC3339), e.String()),
 	}
 }
 
-func LatLon(lat float64, lon float64, e binary.ByteOrder) Pattern {
+func LatLon(lat float64, lon float64, e binary.ByteOrder) LiteralPattern {
 	buf := make([]byte, 8)
 	binary.BigEndian.PutUint32(buf, uint32(lat*1e6))
 	binary.BigEndian.PutUint32(buf[4:], uint32(lon*1e6))
-	return Pattern{
-		Data:    buf,
-		Desc:    fmt.Sprintf("latlon(%f, %f, %s)", lat, lon, e.String()),
-		Compare: bytes.Equal,
+	return &fromBytes{
+		data: buf,
+		desc: fmt.Sprintf("latlon(%f, %f, %s)", lat, lon, e.String()),
 	}
 }
 
 func AnyBytes(n int) Pattern {
-	return Pattern{
-		Data: make([]byte, n),
-		Desc: fmt.Sprintf("any bytes(%d)", n),
-		Compare: func(got []byte, expected []byte) bool {
-			return len(got) == n
-		},
+	return &anyBytes{
+		len:  n,
+		desc: fmt.Sprintf("any bytes(%d)", n),
 	}
 }
 
-func CString(s string, maxLen int) Pattern {
+func CString(s string, maxLen int) LiteralPattern {
 	if len(s) > maxLen-1 {
 		panic("s is longer than maxLen")
 	}
@@ -107,17 +143,16 @@ func CString(s string, maxLen int) Pattern {
 	buf := make([]byte, maxLen)
 	copy(buf, s)
 	buf[len(s)] = 0
-	return Pattern{
-		Data:    buf,
-		Desc:    fmt.Sprintf("cstring(%q, %d)", s, maxLen),
-		Compare: bytes.Equal,
+	return &fromBytes{
+		data: buf,
+		desc: fmt.Sprintf("cstring(%q, %d)", s, maxLen),
 	}
 }
 
-func BytesFrom(patterns ...Pattern) []byte {
+func BytesFrom(patterns ...LiteralPattern) []byte {
 	var buf bytes.Buffer
 	for _, pattern := range patterns {
-		buf.Write(pattern.Data)
+		buf.Write(pattern.Bytes())
 	}
 	return buf.Bytes()
 }
@@ -128,26 +163,26 @@ func ValidateBytes(
 ) error {
 	var log []string
 	for i, pattern := range patterns {
-		n := len(pattern.Data)
+		n := pattern.Len()
 		if len(got) < n {
 			log = append(
 				log,
-				fmt.Sprintf("%d:%s not enough bytes", i, pattern.Desc),
+				fmt.Sprintf("%d:%s not enough bytes", i, pattern.Desc()),
 			)
 			return poop.New(strings.Join(log, ", "))
 		}
 
-		if !pattern.Compare(got[:n], pattern.Data) {
+		if !pattern.Compare(got[:n]) {
 			log = append(
 				log,
-				fmt.Sprintf("%d:%s 👎", i, pattern.Desc),
+				fmt.Sprintf("%d:%s 👎", i, pattern.Desc()),
 			)
 			return poop.New(strings.Join(log, ", "))
 		}
 
 		log = append(
 			log,
-			fmt.Sprintf("%d:%s 👍", i, pattern.Desc),
+			fmt.Sprintf("%d:%s 👍", i, pattern.Desc()),
 		)
 
 		got = got[n:]
