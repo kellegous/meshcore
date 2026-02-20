e7b1a60005b2ffcce7ab667165468a72fbd8ef99
Kelly Norton <kellegous@gmail.com>
2026-02-20 06:45:41 -0500

    tests

--- notifications_test.go
+++ notifications_test.go
@@ -30,6 +30,12 @@ func TestReadNotification(t *testing.T) {
 
 	fakePublicKey := fakePublicKey(42)
 
+	var fakePublicKeyPrefix [6]byte
+	copy(fakePublicKeyPrefix[:], fakePublicKey.Prefix(6))
+
+	var fakePrivateKey [64]byte
+	copy(fakePrivateKey[:], fakeBytes(64, func(i int) byte { return byte(i + 1) }))
+
 	tests := []struct {
 		Name     string
 		Code     NotificationCode
@@ -136,6 +142,216 @@ func TestReadNotification(t *testing.T) {
 					RadioSf:           6,
 					RadioCr:           8,
 					Name:              "testname",
+				},
+			},
+		},
+		{
+			Name: "Sent",
+			Code: NotificationTypeSent,
+			Data: BytesFrom(
+				Byte(byte(1)),
+				Uint32(1234567890, binary.LittleEndian),
+				Uint32(1000, binary.LittleEndian),
+			),
+			Expected: expected{
+				Notification: &SentNotification{
+					Result:         1,
+					ExpectedAckCRC: 1234567890,
+					EstTimeout:     1000,
+				},
+			},
+		},
+		{
+			Name: "ContactMsgRecv",
+			Code: NotificationTypeContactMsgRecv,
+			Data: BytesFrom(
+				Bytes(fakePublicKeyPrefix[:]...),
+				Byte(byte(2)),
+				Byte(byte(TextTypeSignedPlain)),
+				Time(time.Unix(100, 0), binary.LittleEndian),
+				String("test"),
+			),
+			Expected: expected{
+				Notification: &ContactMsgRecvNotification{
+					ContactMessage: ContactMessage{
+						PubKeyPrefix: fakePublicKeyPrefix,
+						PathLen:      2,
+						TextType:     TextTypeSignedPlain,
+						SenderTime:   time.Unix(100, 0),
+						Text:         "test",
+					},
+				},
+			},
+		},
+		{
+			Name: "ChannelMsgRecv",
+			Code: NotificationTypeChannelMsgRecv,
+			Data: BytesFrom(
+				Byte(byte(1)),
+				Byte(byte(2)),
+				Byte(byte(TextTypeSignedPlain)),
+				Time(time.Unix(100, 0), binary.LittleEndian),
+				String("test"),
+			),
+			Expected: expected{
+				Notification: &ChannelMsgRecvNotification{
+					ChannelMessage: ChannelMessage{
+						ChannelIndex: 1,
+						PathLen:      2,
+						TextType:     TextTypeSignedPlain,
+						SenderTime:   time.Unix(100, 0),
+						Text:         "test",
+					},
+				},
+			},
+		},
+		{
+			Name: "CurrTime",
+			Code: NotificationTypeCurrTime,
+			Data: BytesFrom(
+				Time(time.Unix(100, 0), binary.LittleEndian),
+			),
+			Expected: expected{
+				Notification: &CurrTimeNotification{
+					Time: time.Unix(100, 0),
+				},
+			},
+		},
+		{
+			Name: "NoMoreMessages",
+			Code: NotificationTypeNoMoreMessages,
+			Data: nil,
+			Expected: expected{
+				Notification: &NoMoreMessagesNotification{},
+			},
+		},
+		{
+			Name: "ExportContact",
+			Code: NotificationTypeExportContact,
+			Data: BytesFrom(
+				Bytes(1, 2, 3, 4, 5, 6),
+			),
+			Expected: expected{
+				Notification: &ExportContactNotification{
+					AdvertPacket: []byte{1, 2, 3, 4, 5, 6},
+				},
+			},
+		},
+		{
+			Name: "BatteryVoltage",
+			Code: NotificationTypeBatteryVoltage,
+			Data: BytesFrom(
+				Uint16(12345, binary.LittleEndian),
+			),
+			Expected: expected{
+				Notification: &BatteryVoltageNotification{
+					Voltage: 12345,
+				},
+			},
+		},
+		{
+			Name: "DeviceInfo",
+			Code: NotificationTypeDeviceInfo,
+			Data: BytesFrom(
+				Byte(byte(1)),
+				Bytes(0, 0, 0, 0, 0, 0), // reserved 6 bytes
+				CString("test_x", 12),
+				String("test_y"),
+			),
+			Expected: expected{
+				Notification: &DeviceInfoNotification{
+					DeviceInfo: DeviceInfo{
+						FirmwareVersion:   1,
+						FirmwareBuildDate: "test_x",
+						ManufacturerModel: "test_y",
+					},
+				},
+			},
+		},
+		{
+			Name: "PrivateKey",
+			Code: NotificationTypePrivateKey,
+			Data: BytesFrom(
+				Bytes(fakePrivateKey[:]...),
+			),
+			Expected: expected{
+				Notification: &PrivateKeyNotification{
+					PrivateKey: fakePrivateKey,
+				},
+			},
+		},
+		{
+			Name: "Disabled",
+			Code: NotificationTypeDisabled,
+			Data: nil,
+			Expected: expected{
+				Notification: &DisabledNotification{},
+			},
+		},
+		{
+			Name: "ChannelInfo",
+			Code: NotificationTypeChannelInfo,
+			Data: BytesFrom(
+				Byte(byte(1)),
+				CString("test", 32),
+				Bytes(fakeBytes(16, func(i int) byte { return byte(i + 1) })...),
+			),
+			Expected: expected{
+				Notification: &ChannelInfoNotification{
+					ChannelInfo: ChannelInfo{
+						Index:  1,
+						Name:   "test",
+						Secret: fakeBytes(16, func(i int) byte { return byte(i + 1) }),
+					},
+				},
+			},
+		},
+		{
+			Name: "SignStart",
+			Code: NotificationTypeSignStart,
+			Data: BytesFrom(
+				Byte(byte(0)),
+				Uint32(1024, binary.LittleEndian),
+			),
+			Expected: expected{
+				Notification: &SignStartNotification{
+					MaxSignDataLen: 1024,
+				},
+			},
+		},
+		{
+			Name: "Signature",
+			Code: NotificationTypeSignature,
+			Data: BytesFrom(
+				Bytes(fakePrivateKey[:]...),
+			),
+			Expected: expected{
+				Notification: &SignatureNotification{
+					Signature: fakePrivateKey,
+				},
+			},
+		},
+		{
+			Name: "Advert",
+			Code: NotificationTypeAdvert,
+			Data: BytesFrom(
+				Bytes(fakePublicKey.Bytes()...),
+			),
+			Expected: expected{
+				Notification: &AdvertNotification{
+					PublicKey: fakePublicKey,
+				},
+			},
+		},
+		{
+			Name: "PathUpdated",
+			Code: NotificationTypePathUpdated,
+			Data: BytesFrom(
+				Bytes(fakePublicKey.Bytes()...),
+			),
+			Expected: expected{
+				Notification: &PathUpdatedNotification{
+					PublicKey: fakePublicKey,
 				},
 			},
 		},
